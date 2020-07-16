a575e89ad4b023c80e8242166c34e9e5c459e841
sergey.sergeev155 <sergey.sergeev155@gmail.com>
2020-07-16 06:26:47 +0400

    meh

--- sqldb.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-
-class sqldb:
-    def __init__(self, db_file):
-        """Подключение к БД и сохраняем курсор"""
-        self.connection = sqlite3.connect(db_file)
-        self.cursor = self.connection.cursor()
